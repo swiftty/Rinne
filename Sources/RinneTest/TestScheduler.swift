5b4dd424ff488970108e6de7d6851c1dbd63d3ee
swiftty <62803132+swiftty@users.noreply.github.com>
2020-09-26 13:19:48 +0900

    implement TestScheduler

--- Sources/RinneTest/TestScheduler.swift
+++ Sources/RinneTest/TestScheduler.swift
@@ -4,8 +4,13 @@ public final class TestScheduler<SchedulerTimeType, SchedulerOptions>: Scheduler
 where SchedulerTimeType: Strideable,
       SchedulerTimeType.Stride: SchedulerTimeIntervalConvertible {
 
-    public var now: SchedulerTimeType
-    public var minimumTolerance: SchedulerTimeType.Stride = 0
+    private typealias Tick = UInt64
+
+    public private(set) var now: SchedulerTimeType
+    public let minimumTolerance: SchedulerTimeType.Stride = 0
+
+    private var lastTick = 0 as Tick
+    private var queue: [(tick: Tick, date: SchedulerTimeType, action: () -> Void)] = []
 
     public init(now: SchedulerTimeType) {
         self.now = now
@@ -13,14 +18,14 @@ where SchedulerTimeType: Strideable,
 
     public func schedule(options: SchedulerOptions?,
                          _ action: @escaping () -> Void) {
-        fatalError()
+        enqueue(tick: nextTick(), date: now, action: action)
     }
 
     public func schedule(after date: SchedulerTimeType,
                          tolerance: SchedulerTimeType.Stride,
                          options: SchedulerOptions?,
                          _ action: @escaping () -> Void) {
-        fatalError()
+        enqueue(tick: nextTick(), date: date, action: action)
     }
 
     public func schedule(after date: SchedulerTimeType,
@@ -28,6 +33,60 @@ where SchedulerTimeType: Strideable,
                          tolerance: SchedulerTimeType.Stride,
                          options: SchedulerOptions?,
                          _ action: @escaping () -> Void) -> Cancellable {
-        fatalError()
+        let tick = nextTick()
+
+        var scheduleAction: ((SchedulerTimeType) -> () -> Void)!
+        scheduleAction = { [weak self] date in
+            return {
+                let next = date.advanced(by: interval)
+                self?.enqueue(tick: tick, date: next, action: scheduleAction(next))
+                action()
+            }
+        }
+
+        enqueue(tick: tick, date: date, action: scheduleAction(date))
+
+        return AnyCancellable { [weak self] in
+            self?.dequeue(for: tick)
+        }
+    }
+
+    private func enqueue(tick: Tick, date: SchedulerTimeType, action: @escaping () -> Void) {
+
+    }
+
+    private func dequeue(for tick: Tick) {
+        queue.removeAll(where: { $0.tick == tick })
+    }
+
+    private func nextTick() -> Tick {
+        lastTick += 1
+        return lastTick
+    }
+}
+
+extension TestScheduler {
+    public func consume() {
+        while let date = queue.first?.date {
+            consume(until: date)
+        }
+    }
+
+    public func consume(until end: SchedulerTimeType) {
+        while now <= end {
+            queue.sort(by: { ($0.date, $0.tick) < ($1.date, $1.tick) })
+
+            guard let next = queue.first?.date, end >= next else {
+                now = end
+                return
+            }
+
+            now = next
+
+            while let (_, date, action) = queue.first, date == next {
+                queue.removeFirst()
+                action()
+            }
+        }
     }
 }
