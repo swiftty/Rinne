4d125236a95ca2764eda1fbb81d0050bba7706a1
swiftty <62803132+swiftty@users.noreply.github.com>
2020-09-27 17:05:49 +0900

    add Effect

---
+++ Sources/Rinne/Effect.swift
@@ -0,0 +1,45 @@
+import Combine
+
+public struct Effect<Output, Failure: Error>: Publisher {
+    public let upstream: AnyPublisher<Output, Failure>
+
+    public func receive<S>(subscriber: S)
+    where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        upstream.subscribe(subscriber)
+    }
+}
+
+extension Effect {
+    public init<P: Publisher>(_ publisher: P)
+    where Output == P.Output, Failure == P.Failure {
+        upstream = publisher.eraseToAnyPublisher()
+    }
+
+    public init(value: Output) {
+        self.init(Just(value).setFailureType(to: Failure.self))
+    }
+
+    public init(error: Failure) {
+        self.init(Fail(error: error))
+    }
+}
+
+extension Effect {
+    public static func future(_ completion: @escaping (@escaping (Result<Output, Failure>) -> Void) -> Void) -> Self {
+        Deferred {
+            Future { callback in
+                completion { result in callback(result) }
+            }
+        }
+        .eraseToEffect()
+    }
+}
+
+extension Publisher {
+    public func eraseToEffect() -> Effect<Output, Failure> {
+        if let effect = self as? Effect<Output, Failure> {
+            return effect
+        }
+        return Effect(self)
+    }
+}
